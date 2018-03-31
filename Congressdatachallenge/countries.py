b26fbf5ba721c8b776a828f13907fa55397ed200
CorkCastle <carternielsen4@gmail.com>
2018-03-31 19:21:26 -0400

    Text analysis
    Finished text analysis and more complete country list

--- Congressdatachallenge/countries.py
+++ Congressdatachallenge/countries.py
@@ -1,10 +1,12 @@
+import operator
 import json
 from itertools import chain
 import pycountry
 from urllib.request import Request, urlopen
 import re
+from datetime import datetime
 
-#Add names
+#Country name list
 country_list = [country.name for country in list(pycountry.countries)]
 
 #Add official names
@@ -17,59 +19,107 @@ country_list.extend(country_official_names)
 
 #Add historical names
 historic_names = [country.name for country in list(pycountry.historic_countries)]
+
 country_list.extend(historic_names)
 
 #Add missing names
-missing_countries = ['Republic of Kosovo', 'Federated States of Micronesia', 'ST. VINCENT AND THE GRENADINES']
+missing_countries = ['Republic of Kosovo', 'Federated States of Micronesia', 'ST. VINCENT AND THE GRENADINES', 'Union of Soviet Socialist Republics']
+
 country_list.extend(missing_countries)
-alternate_names_of_countries = {'Russian Federation': ('Russia',)}
 
 #Remove redundant names
 country_list.remove('United States')
-
-#print(len(country_list))
+country_list.remove('United States of America')
 
 with open('treaty.json') as json_data:
+    
+    #Treaty list
     treaties = json.load(json_data)
     
     count = 0
-    for i in range(280, 285):
+
+    for i in range(0, len(treaties)):
         
         treaty = treaties[i]
 
-        print("\nREADING TREATY: " + treaty["Title"])
+        print("\nreading treaty: " + treaty["Title"])
 
-        #Get URL
-        url = treaty["URL"] + "/document-text?"
-        print("\trequest sent to:", url)
-        req = Request(url)
-        req.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36")
-        try:
-            resp = urlopen(req)
-        except:
-            print("COULDN'T FIND TEXT")
+        #Attempt to find text if date after 1995 
+        year = int(treaty["Date Received From President"].split("/")[2])
 
-        content = resp.read().decode("utf-8")
+        #Country match
+        match = "n/a"
+        
+        if year > 1995:
 
-        #Text document stored inbetween <pre></pre> tags
-        output = re.compile('<pre>(.*?)</pre>', re.DOTALL | re.IGNORECASE).findall(content)
+            #Fetch url and request treaty text
+            url = treaty["URL"] + "/document-text?"
+            req = Request(url)
+            req.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36")
 
-        #If match found
-        if output:
-            print("\ttreaty text found")
-            treaty_text = output[0]
+            #Attempt GET request
+            try:
+                resp = urlopen(req)
+            except:
+                print("\tcould not find treaty text")
 
-            countries = []
-            for country_name in country_list:
-                country_lc = country_name.lower()
-                if country_lc in treaty_text.lower():
+            content = resp.read().decode("utf-8")
+
+            #If content found extract text between <pre></pre> tags
+            output = re.compile('<pre>(.*?)</pre>', re.DOTALL | re.IGNORECASE).findall(content)
+
+            if output:
+                treaty_text = output[0]
+
+                #Frequency list of country matches
+                countries = {}
+
+                for country_name in country_list:
+                    
+                    country_lc = country_name.lower()
+                    
+                    #Find number of matches of substring
+                    #TODO
+                    match_count = treaty_text.lower().count(country_lc)
+                    
+                    if match_count > 0:
+                        countries[country_name] = match_count
+
+                #Print frequency results
+                if len(countries) > 0:
+                    match = max(countries.items(), key = operator.itemgetter(1))[0]
+                    print("\ttext matched with " + match)
                     count += 1
-                    countries.append(country_name)
-                    print("\t\t...match found: " + country_name)  
-        else:
-            print("\t cound't find text")
-            #treaty['countries'] = countries     
-#print(count) 
+        
+                else:
+                    print("\tno text match found")
+            else:
+                print("\tcound't find treaty text")
 
-#with open('treaties_with_countries.json', 'w') as json_output_data:
-#    json.dump(treaties, json_output_data)
+        #Search title if text matches not found
+        #TODO prioritize title matches over text
+        if match == "n/a":
+
+            #Matches
+            countries = []
+
+            for country_name in country_list:
+                    country_lc = country_name.lower()
+                    if country_lc in treaty["Title"].lower():
+                        
+                        countries.append(country_name)
+
+            #Return longest match
+            if len(countries) > 0:
+                match = max(countries, key = len)
+                print("\ttitle matched with " + match)
+                count += 1
+            else:
+                print("\tno title match found")
+
+        treaties[i]["countries"] = [match]
+
+    print("\n\ntotal " + str(count) + " treaties matched\n\n")
+
+with open('treaties_with_countries.json', 'w') as json_output_data:
+    json.dump(treaties, json_output_data)
